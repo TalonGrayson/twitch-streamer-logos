5343f432b8dd8e424334cdead0381dd2f61dd345
TalonGrayson <talongrayson@gmail.com>
2019-01-18 18:03:45 +0000

    First Commit

---
+++ keys_prod.js
@@ -0,0 +1,10 @@
+module.exports = {
+  twitch: {
+    TWITCH_CLIENT_ID: process.env.TWITCH_CLIENT_ID,
+    TWITCH_CLIENT_SECRET: process.env.TWITCH_CLIENT_SECRET
+  },
+  mongo: {
+    MONGO_USERNAME: process.env.MONGO_USERNAME,
+    MONGO_PASSWORD: process.env.MONGO_PASSWORD
+  }
+};
