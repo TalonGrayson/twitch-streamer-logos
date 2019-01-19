3a31e44fb298f63e7b4668e5f565b4fbcf734549
TalonGrayson <talongrayson@gmail.com>
2019-01-19 00:51:14 +0000

    Included staging in keys conditions

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,4 @@
-if (process.env.NODE_ENV == "production") {
+if (process.env.NODE_ENV == "production" || process.env.NODE_ENV == "staging") {
   module.exports = require("./keys_prod");
 } else {
   module.exports = require("./keys_dev");
--- config/redirect_uri.js
+++ config/redirect_uri.js
@@ -1,15 +1,19 @@
-if (process.env.NODE_ENV == "staging") {
-  module.exports = {
-    redirect_uri:
-      "https://twitch-streamer-logos-stage.herokuapp.com/auth/twitch/redirect"
-  };
-} else if (process.env.NODE_ENV == "production") {
-  module.exports = {
-    redirect_uri:
-      "https://twitch-streamer-logos.herokuapp.com/auth/twitch/redirect"
-  };
-} else {
-  module.exports = {
-    redirect_uri: "http://localhost:5000/auth/twitch/redirect"
-  };
+switch (process.env.NODE_ENV) {
+  case "staging":
+    module.exports = {
+      redirect_uri:
+        "https://twitch-streamer-logos-stage.herokuapp.com/auth/twitch/redirect"
+    };
+    break;
+  case "production":
+    module.exports = {
+      redirect_uri:
+        "https://twitch-streamer-logos.herokuapp.com/auth/twitch/redirect"
+    };
+    break;
+  default:
+    module.exports = {
+      redirect_uri: "http://localhost:5000/auth/twitch/redirect"
+    };
+    break;
 }
